b993534e5c2d1e034115dbc6ab181bf480933975
tejasbadadare <tejasbadadare@gmail.com>
2016-06-15 00:23:34 -0700

    Added output to UI

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-console.log("hello");
 removeDuplicates();
 
 function removeDuplicates () {
@@ -9,9 +8,15 @@ function removeDuplicates () {
     console.log("Tabs array length: " + tabs.length);
 
     var dupIDs = [];     // List of tabId's of duplicates found
+    var dupNames = [];   // List of tabId's of duplicates found
+    var dupCount = [];
+
 
     // Cycle through all tabs and check for duplicates
     for (var i = 0; i < tabs.length; i++) {
+      duplicateName = "";
+      duplicateCount = 0;
+
       for (var j = 0; j < tabs.length; j++) {
         // Skip current tab when searching for duplicates
 
@@ -31,16 +36,40 @@ function removeDuplicates () {
         if (tabs[i].url === tabs[j].url) {
           console.log("Duplicate found, adding:" + j);
           console.log("\t" + tabs[j].url);
+
+          duplicateName = tabs[j].title;
+          duplicateCount++;
+
           dupIDs.push(tabs[j].id);
           delete tabs[j];
         }
       }
+      dupNames[i] = duplicateName;
+      dupCount[i] = duplicateCount;
     }
-
-    console.log("DupID's: " + dupIDs);
 
     // Remove the tabs
     chrome.tabs.remove(dupIDs);
 
+    printResults(dupNames, dupCount);
+
   });
+}
+
+function printResults(dupNames, dupCount) {
+  var out = "";
+  for (var i = 0; i < dupNames.length; i++) {
+    if (dupCount[i] == 1) {
+      out += dupCount[i] + " duplicate of " + dupNames[i] + "\n";
+    } else if (dupCount[i] != 0) {
+      out += dupCount[i] + " duplicates of " + dupNames[i] + "\n";
+    }
+  }
+
+  if (out === "") {
+    document.getElementById("title").innerHTML = "No duplicates found!";
+  }
+
+  document.getElementById("text").innerHTML = out;
+
 }
