51606c93943b4e0556ab56790b54020efc45fe10
tejasbadadare <tejasbadadare@gmail.com>
2016-06-15 00:28:55 -0700

    Added debug statements. Working build.

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ function removeDuplicates () {
   var queryInfo = {windowId: chrome.windows.WINDOW_ID_CURRENT};
   chrome.tabs.query(queryInfo, function(tabs) {
 
-    console.log("Tabs array length: " + tabs.length);
+    console.debug("Tabs array length: " + tabs.length);
 
     var dupIDs = [];     // List of tabId's of duplicates found
     var dupNames = [];   // List of tabId's of duplicates found
@@ -18,24 +18,24 @@ function removeDuplicates () {
       duplicateCount = 0;
 
       for (var j = 0; j < tabs.length; j++) {
-        // Skip current tab when searching for duplicates
 
-        console.log("Looking at (j)" + j + "from (i) " + i);
+        // Skip current tab when searching for duplicates
+        console.debug("Looking at (j)" + j + "from (i) " + i);
 
         if (j == i) {
-          console.log("On current("+i+") Skipping.");
+          console.debug("On current("+i+") Skipping.");
           continue;
         }
 
         // Check for undefined object (already deleted duplicate)
         if (!tabs[j] || !tabs[i]) {
-          console.log("On prev deleted ("+j+") Skipping.");
+          console.debug("On prev deleted ("+j+") Skipping.");
           continue;
         }
 
         if (tabs[i].url === tabs[j].url) {
-          console.log("Duplicate found, adding:" + j);
-          console.log("\t" + tabs[j].url);
+          console.debug("Duplicate found, adding:" + j);
+          console.debug("\t" + tabs[j].url);
 
           duplicateName = tabs[j].title;
           duplicateCount++;
@@ -56,12 +56,16 @@ function removeDuplicates () {
   });
 }
 
+// Print the results of the duplicate removal.
 function printResults(dupNames, dupCount) {
   var out = "";
   for (var i = 0; i < dupNames.length; i++) {
+    // Print singular result
     if (dupCount[i] == 1) {
       out += dupCount[i] + " duplicate of " + dupNames[i] + "\n";
-    } else if (dupCount[i] != 0) {
+    }
+    // Print multiple results
+    else if (dupCount[i] != 0) {
       out += dupCount[i] + " duplicates of " + dupNames[i] + "\n";
     }
   }
